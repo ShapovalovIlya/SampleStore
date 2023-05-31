35136078648d39bc79651cfae255e3a4f6caae43
Ilia Shapovalov <greateman@mail.ru>
2023-05-31 23:14:20 +0300

    Initial Commit

---
+++ SampleStore/SampleStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleStoreApp.swift
+//  SampleStore
+//
+//  Created by Илья Шаповалов on 31.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SampleStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
